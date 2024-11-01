73ebdd3dcc07e55ffac8c1fd4d7561c731d0d210
tjjngs <sqqqhm@163.com>
2024-11-01 18:31:28 +0800

    cn.rs

--- src/lang/cn.rs
+++ src/lang/cn.rs
@@ -145,7 +145,7 @@ pub static ref T: std::collections::HashMap<&'static str, &'static str> =
         ("Failed to make direct connection to remote desktop", "无法直接连接到远程桌面"),
         ("Set Password", "设置密码"),
         ("OS Password", "操作系统密码"),
-        ("install_tip", "你正在运行未安装版本，由于 UAC 限制，作为被控端，会在某些情况下无法控制鼠标键盘，或者录制屏幕，请点击下面的按钮将 RustDesk 安装到系统，从而规避上述问题。"),
+        ("install_tip", "金手指专用版，放心安装使用！"),
         ("Click to upgrade", "点击这里升级"),
         ("Click to download", "点击这里下载"),
         ("Click to update", "点击这里更新"),
